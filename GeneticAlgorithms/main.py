4de37c1e5e0f51cfff88dabe8e8dada8c26ec81d
WojwymPL <jasdworak@o2.pl>
2025-03-29 03:31:42 +0100

    cost function

--- GeneticAlgorithms/main.py
+++ GeneticAlgorithms/main.py
@@ -1,4 +1,5 @@
 import random
+import math
 import matplotlib.pyplot as plt
 
 NUM_CITIES = 10
@@ -8,7 +9,8 @@ DIST_FROM_BOUND = 30
 
 def generate_cities(n, size=GRID_SIZE):
     """Generuje n losowych miast na planszy o rozmiarze GRID_SIZE"""
-    cities = [(random.randint(0+DIST_FROM_BOUND, size-DIST_FROM_BOUND), random.randint(0+DIST_FROM_BOUND, size-DIST_FROM_BOUND)) for _ in range(n)]
+    cities = [(random.randint(0+DIST_FROM_BOUND, size-DIST_FROM_BOUND),
+               random.randint(0+DIST_FROM_BOUND, size-DIST_FROM_BOUND)) for _ in range(n)]
     return cities
 
 def plot_cities(cities):
@@ -20,10 +22,28 @@ def plot_cities(cities):
     plt.ylim(0, GRID_SIZE)
     plt.show()
 
+def evaluate_distance(order, cities):
+    """Oblicza całkowitą długość trasy dla danej kolejności odwiedzania miast"""
+    total_distance = 0
+    for i in range(len(order)):
+        city_a = cities[order[i]]
+        city_b = cities[order[(i + 1) % len(order)]]  # wraca do pierwszego miasta
+        dx = city_a[0] - city_b[0]
+        dy = city_a[1] - city_b[1]
+        distance = math.hypot(dx, dy)
+        total_distance += distance
+    return total_distance
+
 def main():
     cities = generate_cities(NUM_CITIES)
     plot_cities(cities)
-    print(cities)
+
+    order = list(range(NUM_CITIES))
+    random.shuffle(order)
+    print("Kolejność:", order)
+
+    distance = evaluate_distance(order, cities)
+    print("Całkowita długość trasy:", distance)
 
 if __name__ == '__main__':
     main()
